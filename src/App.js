92777d41b91ffb115ac06b272f49910191e051c5
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-25 09:51:49 -0300

    feat: TextField component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from './components/Banner/Banner';
+import TextField from './components/TextField/TextField';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <TextField/>
     </div>
   );
 }
