56d45d3b143f493cf43314d1bc4cfbfd8af42f1a
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-28 15:59:34 -0300

    feat: Form style

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,9 @@
+import "./Form.css"
 import TextField from "../TextField"
 
 const Form = () => {
     return (
-        <section>
+        <section className="formContainer">
             <form>
             <h2>Preencha os dados para criar o card do colaborador.</h2>
             <TextField label = "Nome" placeholder="Digite seu nome"/>
