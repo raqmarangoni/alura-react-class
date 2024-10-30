f80444f4f7828273cab9430fb565a8767dd7b507
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-30 00:22:43 -0300

    feat: DropDownList style

--- src/components/DropDownList/index.js
+++ src/components/DropDownList/index.js
@@ -1,6 +1,8 @@
+import "./DropDownList.css"
+
 const DropDownList = (props) => {
     return (
-        <div>
+        <div className="drop-down-list">
             <label>{props.label}</label>
             <select>
                 {props.items.map(item => <option key={item}>{item}</option>)}
