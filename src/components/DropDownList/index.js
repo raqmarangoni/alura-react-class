ff1ea6aa19c60a4d22694d9f1471070a4de79a8e
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-30 10:27:42 -0300

    feat: add required fields

--- src/components/DropDownList/index.js
+++ src/components/DropDownList/index.js
@@ -4,7 +4,7 @@ const DropDownList = (props) => {
     return (
         <div className="drop-down-list">
             <label>{props.label}</label>
-            <select>
+            <select required={props.required}>
                 {props.items.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -15,14 +15,19 @@ const Form = () => {
         "Inovação e Gestão"
     ]
     
+    const onSave = (event) => { 
+        event.preventDefault() 
+        console.log("Form foi submetido") 
+    }
+
     return (
         <section className="formContainer">
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <TextField label="Nome" placeholder="Digite seu nome" />
-                <TextField label="Cargo" placeholder="Digite seu cargo" />
+                <TextField required={true} label="Nome" placeholder="Digite seu nome" />
+                <TextField required={true} label="Cargo" placeholder="Digite seu cargo" />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-                <DropDownList label="Time" items={teams} />
+                <DropDownList required={true} label="Time" items={teams} />
                 <Button text="Criar Card"/>
             </form>
         </section>
