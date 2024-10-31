9802bb1c6280acc729fd1d8a0c01ab6b9950efde
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-31 11:58:16 -0300

    feat: add Collaborator component style

--- src/components/Collaborator/index.js
+++ src/components/Collaborator/index.js
@@ -2,11 +2,11 @@ import "./Collaborator.css"
 
 const Collaborator = () => {
     return (
-        <div>
-            <div >
+        <div className="collaborator">
+            <div className="header">
                 <img src="http://github.com/raqmarangoni.png" alt="Raquel Marangoni"/>
             </div>
-            <div>
+            <div className="">
                 <h4>Raquel</h4>
                 <h5>Desenvolvedora</h5>
             </div>
