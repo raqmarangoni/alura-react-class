043a216d4e282c2e84c14b6f63d782d42051ca20
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-31 11:57:31 -0300

    feat: add Collaborator component

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import "./Collaborator.css"
+
+const Collaborator = () => {
+    return (
+        <div>
+            <div >
+                <img src="http://github.com/raqmarangoni.png" alt="Raquel Marangoni"/>
+            </div>
+            <div>
+                <h4>Raquel</h4>
+                <h5>Desenvolvedora</h5>
+            </div>
+        </div>
+    )   
+}
+
+export default Collaborator
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,13 +1,15 @@
+import Collaborator from "../Collaborator"
 import "./Team.css"
 
 const Team = (props) => {
-    
+
     const backgroundColor = { backgroundColor: props.secondaryColor }
     const borderColor = { borderColor: props.primaryColor }
 
     return (
         <section className="team" style={backgroundColor}>
             <h3 style={borderColor}>{props.name}</h3>
+            <Collaborator />
         </section>
     )
 }
