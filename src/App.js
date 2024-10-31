f8efc3bdafddaf93b04ea8df792d84fadf8f4de7
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-31 11:31:49 -0300

    feat: add a teams array with dynamic styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,45 @@ import { useState } from "react";
 import Team from "./components/Team";
 
 function App() {
+
+  const teams = [
+    {
+      name: "Programação",
+      primaryColor: "#57C278",
+      secondaryColor: "#D9F7E9"
+    },
+    {
+      name: 'Front-End',
+      primaryColor: '#82CFFA',
+      secondaryColor: '#E8F8FF',
+    },
+    {
+      name: 'Data Science',
+      primaryColor: '#A6D157',
+      secondaryColor: '#F0F8E2',
+    },
+    {
+      name: 'Devops',
+      primaryColor: '#E06B69',
+      secondaryColor: '#FDE7E8',
+    },
+    {
+      name: 'UX e Design',
+      primaryColor: '#D86EBF',
+      secondaryColor: '#FAE5F5',
+    },
+    {
+      name: 'Mobile',
+      primaryColor: '#FEBA05',
+      secondaryColor: '#FFF5D9',
+    },
+    {
+      name: 'Inovação e Gestão',
+      primaryColor: '#FF8A29',
+      secondaryColor: '#FFEEDF',
+    }
+  ]
+
   const [collaborators, setCollaborators] = useState([])
   const onAddCollaborator = (collaborator) => {
     setCollaborators([...collaborators, collaborator]) //agora temos um novo array, espalhamos os colaboradores antigos e adicionando um novo colaborador no final
@@ -11,15 +50,15 @@ function App() {
 
   return (
     <div className="App">
-      <Banner/>
-      <Form  onRegisteredCollaborator={collaborator => onAddCollaborator(collaborator)}/>
-      <Team name="Programação" />
-      <Team name="Front-End" />
-      <Team name="Data Science" />
-      <Team name="UX e Design" />
-      <Team name="Devops" />
-      <Team name="Mobile" />
-      <Team name="Inovação e Gestão" />
+      <Banner />
+      <Form onRegisteredCollaborator={collaborator => onAddCollaborator(collaborator)} />
+      {teams.map(team =>
+        <Team
+          key={team.name}
+          name={team.name}
+          primaryColor={team.primaryColor}
+          secondaryColor={team.secondaryColor}
+          />)}
     </div>
   );
 }
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,13 @@
 import "./Team.css"
 
 const Team = (props) => {
+    
+    const backgroundColor = { backgroundColor: props.secondaryColor }
+    const borderColor = { borderColor: props.primaryColor }
+
     return (
-        <section className="team">
-            <h3>{props.name}</h3>
+        <section className="team" style={backgroundColor}>
+            <h3 style={borderColor}>{props.name}</h3>
         </section>
     )
 }
