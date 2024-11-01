9a853a948d7c8024aedef7c8e87c722201430f3d
Raquel Marangoni <rmarangonisan@gmail.com>
2024-11-01 01:17:38 -0300

    feat: add empty option as initial value of dropdown list

--- src/components/DropDownList/index.js
+++ src/components/DropDownList/index.js
@@ -5,6 +5,7 @@ const DropDownList = (props) => {
         <div className="drop-down-list">
             <label>{props.label}</label>
             <select onChange={event => props.onChangedValue(event.target.value)} required={props.required} value={props.value}>
+                <option value=""></option>
                 {props.items.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
