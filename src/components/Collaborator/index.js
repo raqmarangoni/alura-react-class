699b79a2c3ca6b8492b2230d9eda0329dae7256b
Raquel Marangoni <rmarangonisan@gmail.com>
2024-11-01 01:33:08 -0300

    refactor: destructure props object

--- src/components/Collaborator/index.js
+++ src/components/Collaborator/index.js
@@ -1,17 +1,17 @@
 import "./Collaborator.css"
 
-const Collaborator = (props) => {
+const Collaborator = ({ name, image, position }) => {
     return (
         <div className="collaborator">
             <div className="header">
-                <img src={props.image} alt={props.name}/>
+                <img src={image} alt={name} />
             </div>
             <div className="footer">
-                <h4>{props.name}</h4>
-                <h5>{props.position}</h5>
+                <h4>{name}</h4>
+                <h5>{position}</h5>
             </div>
         </div>
-    )   
+    )
 }
 
 export default Collaborator
